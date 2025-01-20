fcb52ba7d8ed99e769cf92d1678ba8be9fe3276f
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-01-20 19:58:18 +0100

    start progetto e dipendenze installate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LoginPage from "./Components/LoginPage";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
