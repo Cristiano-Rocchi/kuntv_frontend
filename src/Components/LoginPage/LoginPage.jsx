226c96d97d0ab998771bbf518d126f5361842dfb
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-01-22 18:59:57 +0100

    add tv backround animated

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
-import "./LoginPage.css";
+import React, { useState, useRef } from "react";
+import "./LoginPage.scss";
 import BackImgLogin from "../../Assets/LoginPage/Img/back-img-login.jpg";
+import VideoFile from "../../Assets/LoginPage/Vid/prova.mp4";
 
 const LoginPage = () => {
+  const [inputValue, setInputValue] = useState("");
   const [isFocused, setIsFocused] = useState(false);
+  const formRef = useRef(null);
+
+  // Gestisce il clic fuori dal form
+  const handleClickOutside = (event) => {
+    if (formRef.current && !formRef.current.contains(event.target)) {
+      setInputValue(""); // Resetta il testo digitato
+      setIsFocused(false); // Rimuove lo stato attivo del form
+    }
+  };
+
+  // listener per i click globali
+  React.useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div
@@ -15,16 +34,36 @@ const LoginPage = () => {
           backgroundImage: `url(${BackImgLogin})`,
         }}
       ></div>
-      <div className="form-secret">
+      {/* Nuovo div per la 'I' */}
+      <div className="tv-icon">
+        <span>INFO</span>
+        <video
+          className="hover-video"
+          src={VideoFile} // Sostituisci con il percorso del tuo video
+          muted
+          loop
+          autoPlay
+          playsInline
+        ></video>
+      </div>
+
+      <div className="form-secret" ref={formRef}>
         <form>
           <input
             className="input-box"
             type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
             onFocus={() => setIsFocused(true)}
             onBlur={() => setIsFocused(false)}
           />
           <span className="placeholder-text">Enter a secret</span>
         </form>
+        {inputValue && (
+          <button className="submit-button-login" type="button">
+            Invia
+          </button>
+        )}
       </div>
     </div>
   );
