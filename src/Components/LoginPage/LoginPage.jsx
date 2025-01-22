b0f8078f53218262d680b7d61889357986ae4282
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-01-22 16:28:50 +0100

    animation background black and form border

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPage.css";
 import BackImgLogin from "../../Assets/LoginPage/Img/back-img-login.jpg";
 
 const LoginPage = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <>
-      <div className="login-page position-relative">
+      <div
+        className={`login-page position-relative ${isFocused ? "active" : ""}`}
+      >
         <div
-          className="background-login"
+          className={`background-login ${isFocused ? "active" : ""}`}
           style={{
             backgroundImage: `url(${BackImgLogin})`,
           }}
         ></div>
-        <div className=" form-secret  ">
+        <div className="form-secret">
           <form>
-            <input type="text" placeholder="Enter a secret" />
+            <input
+              className="input-box"
+              type="text"
+              placeholder="Enter a secret"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
           </form>
         </div>
       </div>
