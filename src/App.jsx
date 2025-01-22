63c8edcc1e12f0e33125430bf7ce7791dd232cb2
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-01-23 00:25:54 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./Components/LoginPage/LoginPage";
 import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
       <>
+        <Navbar />
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/Home" element={<Home />} />
