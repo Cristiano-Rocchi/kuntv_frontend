2ab3fa0a2bbb4650a901177d8c589940577386ee
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-01-23 22:14:36 +0100

    add underline animation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,30 @@ import "./Home.scss";
 const prova = () => {
   return (
     <div className="prova">
-      <h1></h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
     </div>
   );
 };
--- src/Components/Navbar/MyNavbar.jsx
+++ src/Components/Navbar/MyNavbar.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./MyNavbar.scss";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 const MyNavbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="navbar-wrapper d-flex justify-content-around ">
+    <div
+      className={`navbar-wrapper d-flex justify-content-around ${
+        isScrolled ? "scrolled" : ""
+      }`}
+    >
       <h1 className="logo ms-5 mt-3">Kun</h1>
-      <Navbar expand="lg" className="nav-body" variant="dark">
+      <Navbar expand="lg" className="nav-body">
         <Container>
+          <span></span>
           <Nav className="nav-elements">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">Serie TV</Nav.Link>
