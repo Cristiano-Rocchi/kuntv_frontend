9e3286be5d5c6595e9f72ad627df84198db02b7a
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-01-23 17:18:16 +0100

    navbar clip path

--- src/Components/Navbar/MyNavbar.jsx
+++ src/Components/Navbar/MyNavbar.jsx
@@ -5,20 +5,21 @@ import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const MyNavbar = () => {
   return (
-    <Navbar expand="lg" className="nav-body" variant="dark">
-      <Navbar.Brand href="#" className="me-3">
-        Logo
-      </Navbar.Brand>
-      <span></span>
-      <Container className="d-flex justify-content-center">
-        <Nav className=" nav-elements">
-          <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="#">Link</Nav.Link>
-          <Nav.Link href="#">Link</Nav.Link>
-          <Nav.Link href="#">Link</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <div className="">
+      {" "}
+      <Navbar expand="lg" className="nav-body" variant="dark">
+        <Navbar.Brand href="#" className="me-3"></Navbar.Brand>
+        <span></span>
+        <Container className="d-flex justify-content-center">
+          <Nav className=" nav-elements">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link href="#">Link</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
