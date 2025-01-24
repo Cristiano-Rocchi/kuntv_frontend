49be03882f825e9906c4e36ca0998f937e5c9dfe
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-01-25 00:59:16 +0100

    update

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,10 +1,25 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./Admin.scss";
 const Admin = () => {
   return (
     <div className="body">
-      <div>
-        <h1>Admin</h1>
-      </div>
+      <Container className="d-flex align-items-center">
+        <Row className="w-100 ">
+          <Col md={5} className="section-admin ">
+            <Button className="button-admin">Aggiungi Film</Button>
+            <Button className="button-admin">Aggiungi Serie TV</Button>
+          </Col>
+          <Col md={5} className="section-admin">
+            <div>2</div>
+          </Col>
+          <Col md={5} className="section-admin">
+            <div>3</div>
+          </Col>
+          <Col md={5} className="section-admin">
+            <div>4</div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
